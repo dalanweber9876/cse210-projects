3589e54accfd2ce96e4b581fd093696be2b5d299
Dalan Weber <dalanweber@Dalans-MacBook-Pro.local>
2023-04-24 17:17:17 -0600

    Finished Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+        while (response == "yes")
+        {
+            /*Console.Write("What is the Magic Number?");
+            string numberString = Console.ReadLine();
+            int number = int.Parse(numberString);*/
+            int guess = 0;
+            int score = 0;
+
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+
+            do{
+                Console.Write("What is your guess?");
+                string guessString = Console.ReadLine();
+                guess = int.Parse(guessString);
+
+            
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                if (guess == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                score += 1;
+            } while (guess != number);
+            Console.WriteLine($"You got it in {score} guesses!");
+            Console.Write("Would you like to play again? (Enter 'yes' or 'no')");
+            response = Console.ReadLine();
+        }
     }
 }
